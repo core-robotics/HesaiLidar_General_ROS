49b4d4ce0ac0ff5d8202fad82f793b219ab2ebb8
GihwanJo <aksuvos@gmail.com>
2023-10-20 17:31:10 +0900

    KIAPI 1020
    Change angle

--- src/HesaiLidar_General_SDK/src/PandarGeneralRaw/src/pandarGeneral_internal.cc
+++ src/HesaiLidar_General_SDK/src/PandarGeneralRaw/src/pandarGeneral_internal.cc
@@ -654,7 +654,7 @@ int PandarGeneral_Internal::LoadCorrectionFile(std::string correction_content) {
     }
 
     elev_angle[lineId - 1] = elev;
-    azimuthOffset[lineId - 1] = azimuth - 87.807175926;
+    azimuthOffset[lineId - 1] = azimuth - 88.6974;
   }
   m_sin_elevation_map_.resize(lineCounter);
   m_cos_elevation_map_.resize(lineCounter);
